6effceb0b0ada04f67c399c9a6d70a771d290728
Vinicius dos Santos Verissimo <viniciusdsv93@gmail.com>
2023-03-28 07:54:58 -0300

    feat: implement local strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './local-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller()
 export class AuthController {
@@ -11,6 +11,7 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
   login() {
+    return 'login'
     // return this.authService.login();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,26 @@
-import { Injectable, Post } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
 
+  constructor(private readonly userService: UserService) { }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined
+        }
+      }
+    }
+
+    throw new Error('Invalid credentials')
+  }
 }
