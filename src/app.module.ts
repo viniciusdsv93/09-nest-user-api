b733080bfc811db9f4497ae4e1df184d43ee18b0
Vinicius dos Santos Verissimo <viniciusdsv93@gmail.com>
2023-03-28 08:38:09 -0300

    feat: implement jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [UserModule, PrismaModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard
+  }],
 })
 export class AppModule {}
